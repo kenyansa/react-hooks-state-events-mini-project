8dc6214eab968eb1dda0409c0dd5e5944b779510
kenyansa <kenyansajustine@gmail.com>
2022-05-17 06:13:23 +0300

    update given code for the mini-project

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, onDeletingTask}) {
+  const taskList = tasks.map((task)=>(
+    <Task key = {task.test}
+    text={task.text}
+    category={task.category}
+    onDeletingTask={onDeletingTask}
+    />
+  ));
   return (
-    <div className="tasks">
+    <div className="tasks">{taskList}
       {/* display a list of tasks using Task component */}
+
     </div>
   );
 }
